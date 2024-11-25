8120c5c3dac9f64e462afe88af4e57ee786e942e
Никита Елисеев <elisnikalex@yandex.ru>
2024-11-25 21:17:44 +0300

    tabs instead of spaces

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -111,8 +111,8 @@ struct NRIInterface : public nri::CoreInterface,
 
 
 #define NRI_ABORT_ON_FAILURE(result) \
-    if ((result) != nri::Result::SUCCESS) \
-        exit(1);
+	if ((result) != nri::Result::SUCCESS) \
+		exit(1);
 
 class Sample {
 private:
