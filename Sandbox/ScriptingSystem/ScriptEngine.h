08550d48568ae46977ca82c4a9f2b18ed3ec3601
Vamiro <Vovka00821@yandex.ru>
2024-12-22 16:36:22 +0300

    - started ScriptEngine

---
+++ Sandbox/ScriptingSystem/ScriptEngine.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <filesystem>
+
+class ScriptEngine
+{
+public:
+	static void Init();
+	static void Shutdown();
+
+private:
+	static void InitMono();
+	static void ShutdownMono();
+};
