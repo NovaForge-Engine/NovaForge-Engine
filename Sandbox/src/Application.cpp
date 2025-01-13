c327f587edf5cd588f4da84a835d6e244b2e33f3
Никита Елисеев <elisnikalex@yandex.ru>
2025-01-13 21:21:43 +0300

    format Application.cpp( just for jun)

--- Sandbox/src/Application.cpp
+++ Sandbox/src/Application.cpp
@@ -3,7 +3,7 @@
 #include "ImguiThemes.h"
 
 
-Application::Application() 
+Application::Application()
 	: physicsEngine(PhysicsEngine::Get())
 {
 }
@@ -12,10 +12,9 @@ Application::~Application()
 {
 }
 
-bool Application::Init(int argc,char** argv)
+bool Application::Init(int argc, char** argv)
 {
 
-	
 	cmdline::parser cmdLine;
 
 	InitCmdLineDefault(cmdLine);
@@ -25,38 +24,38 @@ bool Application::Init(int argc,char** argv)
 
 	if (cmdLine.exist("help"))
 	{
-		std::cout <<cmdLine.usage() << std::endl;
+		std::cout << cmdLine.usage() << std::endl;
 		return false;
 	}
 
-	if (!parseStatus) {
-		spdlog::error("Failed to parse command line arguments. Error: {} , Usage {}", cmdLine.error(), cmdLine.usage());
+	if (!parseStatus)
+	{
+		spdlog::error(
+			"Failed to parse command line arguments. Error: {} , Usage {}",
+			cmdLine.error(), cmdLine.usage());
 		return false;
 	}
 
 	ReadCmdLineDefault(cmdLine);
 	ReadCmdLine(cmdLine);
 
-
 	window = new Window();
 
 	window->Initialize(1280, 720, cmdLine.get<std::string>("api"));
 
-
 	nri::AdapterDesc bestAdapterDesc = {};
 	uint32_t adapterDecscNum = 1;
-	if(
-		nri::nriEnumerateAdapters(&bestAdapterDesc, adapterDecscNum)!= nri::Result::SUCCESS)
-		{
+	if (
+		nriEnumerateAdapters(&bestAdapterDesc, adapterDecscNum) !=
+		nri::Result::SUCCESS)
+	{
 		return false;
-		};
+	}
 
-
-	nri::GraphicsAPI graphicsAPI = nri::GraphicsAPI::D3D12;
-		if (cmdLine.get<std::string>("api") == "VULKAN" || cmdLine.get<std::string>("api") == "VK")
-	graphicsAPI = nri::GraphicsAPI::VK;
-
-
+	auto graphicsAPI = nri::GraphicsAPI::D3D12;
+	if (cmdLine.get<std::string>("api") == "VULKAN" || cmdLine.get<
+		    std::string>("api") == "VK")
+		graphicsAPI = nri::GraphicsAPI::VK;
 
 	nri::DeviceCreationDesc deviceCreationDesc = {};
 	deviceCreationDesc.graphicsAPI = graphicsAPI;
@@ -68,34 +67,32 @@ bool Application::Init(int argc,char** argv)
 	deviceCreationDesc.adapterDesc = &bestAdapterDesc;
 	deviceCreationDesc.allocationCallbacks = m_AllocationCallbacks;
 
-	
-	if (nri::nriCreateDevice(deviceCreationDesc, m_Device) != nri::Result::SUCCESS)
+	if (nriCreateDevice(deviceCreationDesc, m_Device) != nri::Result::SUCCESS)
 	{
 		return false;
 	}
 
 	if (nri::nriGetInterface(
-			*m_Device, "nri::CoreInterface", sizeof(nri::CoreInterface),
-			(nri::CoreInterface*)&NRI) != nri::Result::SUCCESS)
+		    *m_Device, "nri::CoreInterface", sizeof(nri::CoreInterface),
+			(nri::CoreInterface*)& NRI) != nri::Result::SUCCESS)
 	{
 		return false;
 	}
 	if (nri::nriGetInterface(
-			*m_Device, "nri::HelperInterface", sizeof(nri::HelperInterface),
+		    *m_Device, "nri::HelperInterface", sizeof(nri::HelperInterface),
 			(nri::HelperInterface*)&NRI) != nri::Result::SUCCESS)
 		return false;
-	
+
 	if (nri::nriGetInterface(
-			*m_Device, "nri::StreamerInterface", sizeof(nri::StreamerInterface),
+		    *m_Device, "nri::StreamerInterface", sizeof(nri::StreamerInterface),
 			(nri::StreamerInterface*)&NRI) != nri::Result::SUCCESS)
 		return false;
-	
+
 	if (nri::nriGetInterface(*m_Device, "nri::SwapChainInterface",
-	                          sizeof(nri::SwapChainInterface),
-	                          (nri::SwapChainInterface*)&NRI) !=
+	                    sizeof(nri::SwapChainInterface),
+	                         (nri::SwapChainInterface*)&NRI) !=
 	    nri::Result::SUCCESS)
 		return false;
-	
 
 	nri::StreamerDesc streamerDesc = {};
 	streamerDesc.dynamicBufferMemoryLocation = nri::MemoryLocation::HOST_UPLOAD;
@@ -103,23 +100,21 @@ bool Application::Init(int argc,char** argv)
 	                                      nri::BufferUsageBits::INDEX_BUFFER;
 	streamerDesc.constantBufferMemoryLocation =
 		nri::MemoryLocation::HOST_UPLOAD;
+	streamerDesc.constantBufferSize = DYNAMIC_CONSTANT_BUFFER_SIZE;
 	streamerDesc.frameInFlightNum = BUFFERED_FRAME_MAX_NUM;
 
 	if ((NRI.CreateStreamer(*m_Device, streamerDesc, m_Streamer)) !=
 	    nri::Result::SUCCESS)
 		return false;
-	;
 
 	// Command queue
 	if (NRI.GetCommandQueue(*m_Device, nri::CommandQueueType::GRAPHICS,
-	                         m_CommandQueue) != nri::Result::SUCCESS)
+	                        m_CommandQueue) != nri::Result::SUCCESS)
 		return false;
-	;
 
 	// Fences
 	if (NRI.CreateFence(*m_Device, 0, m_FrameFence) != nri::Result::SUCCESS)
 		return false;
-	;
 
 	nri::Format swapChainFormat;
 	{
@@ -128,13 +123,14 @@ bool Application::Init(int argc,char** argv)
 		swapChainDesc.commandQueue = m_CommandQueue;
 		swapChainDesc.format = nri::SwapChainFormat::BT709_G22_8BIT;
 		swapChainDesc.verticalSyncInterval = window->m_VsyncInterval;
-		swapChainDesc.width = (uint16_t)window->m_RenderOutputWidth;
-		swapChainDesc.height = (uint16_t)window->m_RenderOutputHeight;
+		swapChainDesc.width = static_cast<uint16_t>(window->
+			m_RenderOutputWidth);
+		swapChainDesc.height = static_cast<uint16_t>(window->
+			m_RenderOutputHeight);
 		swapChainDesc.textureNum = SWAP_CHAIN_TEXTURE_NUM;
 		if ((NRI.CreateSwapChain(*m_Device, swapChainDesc, m_SwapChain)) !=
 		    nri::Result::SUCCESS)
 			return false;
-		
 
 		uint32_t swapChainTextureNum;
 		nri::Texture* const* swapChainTextures =
@@ -151,7 +147,6 @@ bool Application::Init(int argc,char** argv)
 			if ((NRI.CreateTexture2DView(textureViewDesc, colorAttachment)) !=
 			    nri::Result::SUCCESS)
 				return false;
-			;
 
 			const BackBuffer backBuffer = {colorAttachment,
 			                               swapChainTextures[i]};
@@ -162,20 +157,20 @@ bool Application::Init(int argc,char** argv)
 	for (Frame& frame : window->GetFrames())
 	{
 		if (NRI.CreateCommandAllocator(*m_CommandQueue,
-		                                frame.commandAllocator) !=
+		                               frame.commandAllocator) !=
 		    nri::Result::SUCCESS)
 			return false;
-		
+
 		if (NRI.CreateCommandBuffer(*frame.commandAllocator,
-		                             frame.commandBuffer) !=
+		                            frame.commandBuffer) !=
 		    nri::Result::SUCCESS)
 			return false;
-		
+
 	}
 	bool result;
 	MainRenderPass::InitParams passParams = {
 		.NRI = NRI,
-		.helperInterface =NRI,
+		.helperInterface = NRI,
 		.m_Device = *m_Device,
 		.renderTargetFormat = swapChainFormat,
 		.commandQueue = m_CommandQueue,
@@ -196,6 +191,13 @@ bool Application::Init(int argc,char** argv)
 
 	physicsEngine->Init();
 
+
+	//loader.LoadModel(GetFullPath("Sponza/models/sponza.obj", DataFolder::SCENES));
+	//loader.LoadModel(GetFullPath("PZ18.ma", DataFolder::SCENES));
+	//loader.LoadModel(GetFullPath("PZ19.ma", DataFolder::SCENES));
+	//loader.LoadModel(GetFullPath("fhead.stl", DataFolder::SCENES));
+
+
 	return true;
 }
 
@@ -210,11 +212,10 @@ void Application::Update()
 {
 	glfwPollEvents();
 
-
-	if (glfwWindowShouldClose(window->GetGLFWWindow())) {
+	if (glfwWindowShouldClose(window->GetGLFWWindow()))
+	{
 		shouldClose = false;
 	}
-
 
 	//(@Tenzy21) Note: Updating physics at the end of the frame. Everything else is updating above
 	physicsEngine->Update();
@@ -223,11 +224,11 @@ void Application::Update()
 void Application::Draw()
 {
 	const uint32_t bufferedFrameIndex = frameIndex % BUFFERED_FRAME_MAX_NUM;
-	const Frame& frame = window->GetFrames()[bufferedFrameIndex]	;
-	nri::CommandBuffer* buffer = frame.commandBuffer;
+	const Frame& frame = window->GetFrames()[bufferedFrameIndex];
+	nri::CommandBuffer* commandBuffer = frame.commandBuffer;
 
-
-	if (frameIndex >= BUFFERED_FRAME_MAX_NUM) {
+	if (frameIndex >= BUFFERED_FRAME_MAX_NUM)
+	{
 		NRI.Wait(*m_FrameFence, 1 + frameIndex - BUFFERED_FRAME_MAX_NUM);
 		NRI.ResetCommandAllocator(*frame.commandAllocator);
 	}
@@ -236,26 +237,34 @@ void Application::Draw()
 		ImGui::Begin("Engine Interface");
 		ImGui::StyleColorsDark();
 		//ImGui::DockSpaceOverViewport(0, ImGui::GetMainViewport());
-		if (ImGui::BeginMenuBar()) {
-			if (ImGui::BeginMenu("File")) {
-				if (ImGui::MenuItem("New Scene")) {
+		if (ImGui::BeginMenuBar())
+		{
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::MenuItem("New Scene"))
+				{
 
 				}
-				if (ImGui::MenuItem("Open Scene")) {
+				if (ImGui::MenuItem("Open Scene"))
+				{
 
 				}
-				if (ImGui::MenuItem("Save Scene")) {
+				if (ImGui::MenuItem("Save Scene"))
+				{
 
 				}
 
 				ImGui::EndMenu();
 			}
 
-			if (ImGui::BeginMenu("Edit")) {
-				if (ImGui::MenuItem("Undo")) {
+			if (ImGui::BeginMenu("Edit"))
+			{
+				if (ImGui::MenuItem("Undo"))
+				{
 
 				}
-				if (ImGui::MenuItem("Redo")) {
+				if (ImGui::MenuItem("Redo"))
+				{
 
 				}
 
@@ -269,11 +278,13 @@ void Application::Draw()
 
 		ImGui::CollapsingHeader("Scene Management");
 		ImGui::Text("Current Scene: Scene1");
-		if (ImGui::Button("Load Scene")) {
+		if (ImGui::Button("Load Scene"))
+		{
 
 		}
 
-		if (ImGui::Button("Save Scene")) {
+		if (ImGui::Button("Save Scene"))
+		{
 
 		}
 
@@ -281,7 +292,8 @@ void Application::Draw()
 
 		static const char* objects[] = {"Sphere", "Cube", "Light"};
 		static int object_current_idx = 0;
-		ImGui::ListBox("Objects", &object_current_idx, objects, IM_ARRAYSIZE(objects), 4);
+		ImGui::ListBox("Objects", &object_current_idx, objects,
+		               IM_ARRAYSIZE(objects), 4);
 
 		ImGui::CollapsingHeader("Object Inspector");
 		{
@@ -296,7 +308,8 @@ void Application::Draw()
 			static float scale[3] = {1.0f, 1.0f, 1.0f};
 			ImGui::InputFloat3("Scale", scale);
 
-			if (ImGui::Button("Delete Object")) {
+			if (ImGui::Button("Delete Object"))
+			{
 
 			}
 		}
@@ -310,7 +323,8 @@ void Application::Draw()
 			static float color[3] = {1.0f, 0.5f, 0.5f};
 			ImGui::ColorEdit3("Color", color);
 
-			if (ImGui::Button("Create Material")) {
+			if (ImGui::Button("Create Material"))
+			{
 			}
 		}
 
@@ -326,60 +340,56 @@ void Application::Draw()
 			ImGui::SliderFloat("Field of View", &cameraFov, 1.0f, 90.0f);
 		}
 	}
-	ApplyTheme(ImGuiTheme::ImGuiTheme_SoDark_AccentRed);
+	ApplyTheme(ImGuiTheme_SoDark_AccentRed);
 
-	uiRenderPass.EndUI(NRI,*m_Streamer);
+	uiRenderPass.EndUI(NRI, *m_Streamer);
 	NRI.CopyStreamerUpdateRequests(*m_Streamer);
 
 	mainRenderPass.PrepareFrame();
 	uiRenderPass.PrepareFrame();
 
-
-    const uint32_t currentTextureIndex =
+	const uint32_t currentTextureIndex =
 		NRI.AcquireNextSwapChainTexture(*m_SwapChain);
 
 	BackBuffer& backBuffer = window->m_SwapChainBuffers[currentTextureIndex];
-
-
-
-	mainRenderPass.Draw(NRI, NRI, NRI, NRI, *buffer, frame, backBuffer,
-	                    bufferedFrameIndex, window->m_RenderWindowWidth,
-	                    window->m_RenderWindowHeight);
-
-
-	nri::AttachmentsDesc attachmentsDesc = {};
-	attachmentsDesc.colorNum = 1;
-	attachmentsDesc.colors = &backBuffer.colorAttachment;
-
-
-	 NRI.CmdBeginRendering(*buffer, attachmentsDesc);
-	{
-		uiRenderPass.Draw(NRI, NRI, *m_Streamer, *buffer, 1.0f, true);
+	{ //MainPass
+		mainRenderPass.Draw(NRI, NRI, NRI, NRI, *commandBuffer, frame, backBuffer,
+							bufferedFrameIndex, window->m_RenderWindowWidth,
+							window->m_RenderWindowHeight);
 	}
-	 NRI.CmdEndRendering(*buffer);
+	
+	{	//UI
+		nri::AttachmentsDesc attachmentsDesc = {};
+		attachmentsDesc.colorNum = 1;
+		attachmentsDesc.colors = &backBuffer.colorAttachment;
 
-	 nri::TextureBarrierDesc textureBarrierDescs = {};
-	 textureBarrierDescs.texture = backBuffer.texture;
-	 textureBarrierDescs.after = {nri::AccessBits::COLOR_ATTACHMENT,
-	                              nri::Layout::COLOR_ATTACHMENT};
-	 textureBarrierDescs.layerNum = 1;
-	 textureBarrierDescs.mipNum = 1;
+		NRI.CmdBeginRendering(*commandBuffer, attachmentsDesc);
+		{
+			uiRenderPass.Draw(NRI, NRI, *m_Streamer, *commandBuffer, 1.0f, true);
+		}
+		NRI.CmdEndRendering(*commandBuffer);
 
-	 nri::BarrierGroupDesc barrierGroupDesc = {};
-	 barrierGroupDesc.textureNum = 1;
-	 barrierGroupDesc.textures = &textureBarrierDescs;
+		nri::TextureBarrierDesc textureBarrierDescs = {};
+		textureBarrierDescs.texture = backBuffer.texture;
+		textureBarrierDescs.after = {nri::AccessBits::COLOR_ATTACHMENT,
+		                             nri::Layout::COLOR_ATTACHMENT};
+		textureBarrierDescs.layerNum = 1;
+		textureBarrierDescs.mipNum = 1;
 
+		nri::BarrierGroupDesc barrierGroupDesc = {};
+		barrierGroupDesc.textureNum = 1;
+		barrierGroupDesc.textures = &textureBarrierDescs;
 
-	 textureBarrierDescs.before = textureBarrierDescs.after;
-	 textureBarrierDescs.after = {nri::AccessBits::UNKNOWN,
-	                              nri::Layout::PRESENT};
+		textureBarrierDescs.before = textureBarrierDescs.after;
+		textureBarrierDescs.after = {nri::AccessBits::UNKNOWN,
+		                             nri::Layout::PRESENT};
 
-	 NRI.CmdBarrier(*buffer, barrierGroupDesc);
+		NRI.CmdBarrier(*commandBuffer, barrierGroupDesc);
+	}
+	NRI.EndCommandBuffer(*commandBuffer);
 
-
-	 NRI.EndCommandBuffer(*buffer);
-
-	  { // Submit
+	{
+		// Submit
 		nri::QueueSubmitDesc queueSubmitDesc = {};
 		queueSubmitDesc.commandBuffers = &frame.commandBuffer;
 		queueSubmitDesc.commandBufferNum = 1;
@@ -387,18 +397,17 @@ void Application::Draw()
 		NRI.QueueSubmit(*m_CommandQueue, queueSubmitDesc);
 	}
 
-
-
 	// Present
 	NRI.QueuePresent(*m_SwapChain);
 
-	{ // Signaling after "Present" improves D3D11 performance a bit
+	{
+		// Signaling after "Present" improves D3D11 performance a bit
 		nri::FenceSubmitDesc signalFence = {};
 		signalFence.fence = m_FrameFence;
 		signalFence.value = 1 + frameIndex;
 
 		nri::QueueSubmitDesc queueSubmitDesc = {};
-		queueSubmitDesc.signalFences = &signalFence;  
+		queueSubmitDesc.signalFences = &signalFence;
 		queueSubmitDesc.signalFenceNum = 1;
 
 		NRI.QueueSubmit(*m_CommandQueue, queueSubmitDesc);
